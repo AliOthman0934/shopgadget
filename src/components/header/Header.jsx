84dd0f6aad9056a95eee9da05d1b35c58cf67b5c
Ali Othman <alioth840@gmail.com>
2025-02-25 19:29:19 +0100

    Add middle header with search box and shoping cart in header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,15 +6,26 @@ const Header = ()=> {
                 <i className="bi bi-list"></i>
             </div>
             <div className="top-headre-log">
-                <i className="bi bi-basket2"></i>
-                ShopGad
+                <i className="bi bi-basket2"> </i>
+                Welcom To ShopGad
             </div>
-            <div className="top-header-text">
+            {/* <div className="top-header-text">
                 Welcom To
-            </div>
+            </div> */}
             <div className="top-header-phone">
                 0031 638 970 170
                 <i className="bi bi-telephone"></i>
+            </div>
+        </div>
+        <div className="middle-header">
+            <div className="middle-header-search-box">
+                <input type="search" name="" id="" placeholder="Find What You Want"/>
+                <button className="search-box-btn">Search</button>
+            </div>
+            <div className="middle-header-shoping-cart">
+                Shoping Cart
+                <i className="bi bi-cart3"></i>
+                <sapn className="cart-notificcation">1</sapn>
             </div>
         </div>
     </header>)
